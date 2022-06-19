c1a6aa49e60b880ae6bd57e2292c035b342fb70f
Nazia <wi21b122@technikum-wien.at>
2022-06-19 17:50:38 +0200

    Initial commit

---
+++ src/main/java/at/technikum/slm_projekt/SlmProjektApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.slm_projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmProjektApplication.class, args);
+    }
+
+}
